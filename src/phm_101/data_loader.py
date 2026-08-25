50df21156baabf204dafc7aa486b6f191cfc5aad
imran-git-000 <chouybat@umd.edu>
2026-08-25 15:33:19 -0400

    set valid end

--- src/phm_101/data_loader.py
+++ src/phm_101/data_loader.py
@@ -75,8 +75,8 @@ class DataLoader:
         # last snapshot to keep
         self.valid_end: dict[str, str] = {
             '1st_test': '2003-11-25 23:39:56',
-            '2nd_test': '2004-02-19 06:22:39',
-            '3rd_test': '2004-04-18 02:42:55',
+            '2nd_test': '2004-02-19 06:02:39',
+            '3rd_test': '2004-04-18 02:32:55',
         }
 
     def save(self) -> None:
